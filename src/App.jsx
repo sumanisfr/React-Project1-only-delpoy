655063462641d20b6b393b4f4da5d06f5bbc5fac
suman <sumankumarghosh94@gmail.com>
2025-06-11 23:59:54 +0530

    all add react bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello Suman</h1>
+      <button>Click me</button>
+      <p>This is a simple React application.</p>
+      <button type="button" class="btn btn-primary">
+        Primary
+      </button>
+      <button type="button" class="btn btn-secondary">
+        Secondary
+      </button>
+      <button type="button" class="btn btn-success">
+        Success
+      </button>
+      <button type="button" class="btn btn-danger">
+        Danger
+      </button>
+      <button type="button" class="btn btn-warning">
+        Warning
+      </button>
+      <button type="button" class="btn btn-info">
+        Info
+      </button>
+      <button type="button" class="btn btn-light">
+        Light
+      </button>
+      <button type="button" class="btn btn-dark">
+        Dark
+      </button>
+
+      <button type="button" class="btn btn-link">
+        Link
+      </button>
+    </div>
+  );
+}
+
+export default App;
